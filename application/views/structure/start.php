c8308f1ac0423eee870c917e900e636aba0ec1ca
andcia <andreciappara@gmail.com>
2018-05-09 17:56:06 +0200

    Implimentation
    commit 1

---
+++ application/views/structure/start.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>MCAST ICA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" style="image: url('mcast.png')" href="home.php">MCAST ICA</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="services.html">
+                  Services</a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="https://outlook.office.com/owa/?realm=mcast.edu.mt#authRedirect=true">Email</a>
+                <a class="dropdown-item" href="http://www.mcast.edu.mt/183">Handbook</a>
+                <a class="dropdown-item" href="http://ict.mcast.edu.mt/wp-content/uploads/DOC-104-REV-D-REQUEST-FOR-CONSIDERATION-OF-EXTENUATING-CIRCUMSTANCES-FOR-EXTENSION-OF-THE-SUBMISSION-DEADLINE-OF-HOME-BASED-ASSIGNMENTS-FORM.pdf">Home Request Form</a>
+                <a class="dropdown-item" href="http://mcast.edu.mt/searchOurCatalogue">Library</a>
+                <a class="dropdown-item" href="https://www.schoology.com/">Schoology</a>
+                <a class="dropdown-item" href="portfolio-item.html">TCA Request Form</a>
+                <a class="dropdown-item" href="http://ict.mcast.edu.mt/wp-content/uploads/Groups-15.pdf">Time Table</a>
+                <a class="dropdown-item" href="http://attendance.mcast.edu.mt/MyAttendance">Student Attendance</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="portfolio.php">
+                Student Portfolio
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Activities
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                <a class="dropdown-item" href="blog-home-1.html">Blog Home 1</a>
+                <a class="dropdown-item" href="blog-home-2.html">Blog Home 2</a>
+                <a class="dropdown-item" href="blog-post.html">Blog Post</a>
+              </div>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact Us</a>
+              </li>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
